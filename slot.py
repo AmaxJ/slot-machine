79d9a1b4c8a2bd3368c1ebd62dbfb3bdfa78f551
alan johnson <ajohnson88@gmail.com>
2015-02-02 02:29:04 -0500

    added functions matrix, reel_values

--- slot.py
+++ slot.py
@@ -11,7 +11,7 @@ class Player:
 	def add_tokens(self, numtokens):
 		self.tokens += numtokens
 
-	def has_balance(self, self.tokens):
+	def has_balance(self):
 		if self.tokens == 0:
 			return False
 		else:
@@ -52,25 +52,56 @@ class Player:
 
 class Slot_machine:
 	def __init__(self):
-		self.values = ["SIX!!!", "LUCKY6", "CHERRY", "3-BAR!","2-BAR!","1-BAR!"]
+		self.values = [(0, 'SIX!!!'), (1, 'LUCKY6'), (2, 'CHERRY'), 
+					   (3, '3-BAR!'), (4, '2-BAR!'), (5, '1-BAR!')]
 		self.multiplier = 1 
 		self.wager = 0    
+		self.map = []
 
-		#wheel spins vertically:
 
 	def valid_bet(self, wagerSize):
 		if wagerSize < 0 or wagerSize > 3:
 			return False
 		else:
 			return True
-			
-	def spin(self):
-		print "You can bet up to 3 tokens per spin: "
-		self.wager = raw_input("Please enter your bet: ")
-		if self.valid_bet(self.wager):
-			#spin
+	
+	def reel_values(self, value):
+		"""Based on the value on display in the middle, calculates the values
+		above and below on the reel"""
+		upper = 0
+		lower = 0
+		if value == 0:
+			upper, lower = 5, 1
+		elif value == 5:
+			upper, lower = 4, 0
+		else:
+			upper, lower = value - 1, value + 1
+		return upper, lower
 
-		pass
+
+
+	def matrix(self):
+		num1, num2, num3 = randrange(6), randrange(6), randrange(6)
+		display = [[],[],[]]
+		for item in display:
+			if item == display[0]:
+				display[0].append(self.reel_values(num1)[0])
+				display[0].append(self.reel_values(num2)[0])
+				display[0].append(self.reel_values(num3)[0])
+			elif item == display[1]:
+				display[1].append(num1)
+				display[1].append(num2)
+				display[1].append(num3)
+			elif item == display[2]:
+				display[2].append(self.reel_values(num1)[1])
+				display[2].append(self.reel_values(num2)[1])
+				display[2].append(self.reel_values(num2)[1])
+		self.map = display
+
+	def rep_matrix(self, matrix):
+		
+		reppped = [[],[],[]]
+
 
 	def diagonal(self):	#checks to see if values match diagonally
 		pass
@@ -83,14 +114,15 @@ class Slot_machine:
 		pass
 
 
-		
+
 
 	
 
 def test():
-
-	assert display[0][0] != [1][0]
-	assert display [0][1] != [1][1]
+	test = Slot_machine()
+	print test.map
+	test.matrix()
+	print test.map
 
 	print "**Create player instance, print amt of tokens(0): "
 	player1 = Player('Alan')
@@ -100,7 +132,7 @@ def test():
 	player1.add_tokens(10)
 	print player1.tokens, "\n"
 
-	print dir(player1)
+	#print dir(player1)
 
 #test()
 
